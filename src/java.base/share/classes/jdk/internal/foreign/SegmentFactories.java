ffa521f4f060eae28da9e3117ac5193fafce96be
Per Minborg <pminborg@gmail.com>
2024-05-24 12:50:51 +0200

    Rework allocation/free

--- src/java.base/share/classes/jdk/internal/foreign/SegmentFactories.java
+++ src/java.base/share/classes/jdk/internal/foreign/SegmentFactories.java
@@ -80,6 +80,24 @@ public class SegmentFactories {
         return new NativeMemorySegmentImpl(min, byteSize, false, MemorySessionImpl.GLOBAL_SESSION);
     }
 
+    /**
+     * {@return a new MemorySegment that can be freed using {@linkplain #free(MemorySegment)}}
+     * <p>
+     * The segment's {@linkplain MemorySegment#maxByteAlignment()} >= 8.
+     *
+     * @param byteSize to allocate
+     */
+    public static MemorySegment allocateSegment(int byteSize) {
+        return makeNativeSegmentUnchecked(allocateMemoryWrapper(byteSize), byteSize);
+    }
+
+    public static void free(MemorySegment segment) {
+        if (segment.scope() != MemorySessionImpl.GLOBAL_SESSION) {
+            throw new IllegalStateException();
+        }
+        UNSAFE.freeMemory(segment.address());
+    }
+
     public static MemorySegment fromArray(byte[] arr) {
         ensureInitialized();
         Objects.requireNonNull(arr);
@@ -169,44 +187,6 @@ public class SegmentFactories {
             segment = segment.asSlice(delta, byteSize);
         }
         return segment;
-    }
-
-    public sealed interface SegmentAllocation {
-
-        MemorySegment segment();
-
-        record SegmentAllocationImpl(@Override MemorySegment segment,
-                                     long buf,
-                                     long alignedSize,
-                                     long byteSize) implements SegmentAllocation {}
-    }
-
-    public static SegmentAllocation segmentAllocation(long byteSize, long byteAlignment) {
-        ensureInitialized();
-        if (VM.isDirectMemoryPageAligned()) {
-            byteAlignment = Math.max(byteAlignment, AbstractMemorySegmentImpl.NIO_ACCESS.pageSize());
-        }
-        long alignedSize = Math.max(1L, byteAlignment > MAX_MALLOC_ALIGN ?
-                byteSize + (byteAlignment - 1) :
-                byteSize);
-
-        AbstractMemorySegmentImpl.NIO_ACCESS.reserveMemory(alignedSize, byteSize);
-
-        long buf = allocateMemoryWrapper(alignedSize);
-        long alignedBuf = Utils.alignUp(buf, byteAlignment);
-        AbstractMemorySegmentImpl segment = new NativeMemorySegmentImpl(buf, alignedSize,
-                false, MemorySessionImpl.GLOBAL_SESSION);
-        if (alignedSize != byteSize) {
-            long delta = alignedBuf - buf;
-            segment = segment.asSlice(delta, byteSize);
-        }
-        return new SegmentAllocation.SegmentAllocationImpl(segment, buf, alignedSize, byteSize);
-    }
-
-    public static void free(SegmentAllocation allocation) {
-        SegmentAllocation.SegmentAllocationImpl alloc = (SegmentAllocation.SegmentAllocationImpl) allocation;
-        UNSAFE.freeMemory(alloc.buf());
-        AbstractMemorySegmentImpl.NIO_ACCESS.unreserveMemory(alloc.alignedSize(), alloc.byteSize());
     }
 
     private static long allocateMemoryWrapper(long size) {
