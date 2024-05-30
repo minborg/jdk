bbf8110e1a92156e9212622b462475c2db9a602f
Per Minborg <pminborg@gmail.com>
2024-05-30 13:53:02 +0200

    Add multithreaded tests

--- test/jdk/jdk/internal/lang/stable/MemoizedFunctionTest.java
+++ test/jdk/jdk/internal/lang/stable/MemoizedFunctionTest.java
@@ -29,17 +29,21 @@
  */
 
 import jdk.internal.lang.StableArray;
+import jdk.internal.lang.StableValues;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 final class MemoizedFunctionTest {
 
@@ -73,6 +77,51 @@ final class MemoizedFunctionTest {
         assertTrue(function.toString().startsWith(expectedEmpty), function.toString());
         function.apply(INDEX);
         assertTrue(function.toString().contains("[" + INDEX + "]"), function.toString());
+    }
+
+    @Test
+    void shakedownSingleThread() {
+        for (int size = 0; size < 128; size++) {
+            Function<Integer, Integer> memo = StableValues.memoizedFunction(IntStream.range(0, size).boxed().collect(Collectors.toSet()), Function.identity());
+            for (int i = 0; i < size; i++) {
+                assertEquals(i, memo.apply(i));
+            }
+        }
+    }
+
+    @Test
+    void shakedownMultiThread() {
+        int threadCount = 8;
+        var startGate = new CountDownLatch(1);
+        for (int size = 0; size < 128; size++) {
+            Function<Integer, Integer> memo = StableValues.memoizedFunction(IntStream.range(0, size).boxed().collect(Collectors.toSet()), Function.identity());
+            final int fSize = size;
+            List<Thread> threads = Stream.generate(() -> new Thread(() -> {
+                        while (startGate.getCount() != 0) {
+                            Thread.onSpinWait();
+                        }
+                        for (int i = 0; i < fSize; i++) {
+                            int value = memo.apply(i);
+                            assertEquals(i, value);
+                        }
+                    }))
+                    .limit(threadCount)
+                    .toList();
+            threads.forEach(Thread::start);
+            // Give some time for the threads to start
+            LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(1));
+            // Release them as close as possible
+            startGate.countDown();
+            threads.forEach(MemoizedFunctionTest::join);
+        }
+    }
+
+    static void join(Thread t) {
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            fail(e);
+        }
     }
 
 }
--- test/jdk/jdk/internal/lang/stable/MemoizedIntFunctionTest.java
+++ test/jdk/jdk/internal/lang/stable/MemoizedIntFunctionTest.java
@@ -29,12 +29,20 @@
  */
 
 import jdk.internal.lang.StableArray;
+import jdk.internal.lang.StableValues;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
 import java.util.function.Function;
 import java.util.function.IntFunction;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -69,6 +77,51 @@ final class MemoizedIntFunctionTest {
         assertTrue(function.toString().startsWith(expectedEmpty), function.toString());
         function.apply(INDEX);
         assertTrue(function.toString().contains("delegate=StableArray[.unset, [" + INDEX + "], .unset]"), function.toString());
+    }
+
+    @Test
+    void shakedownSingleThread() {
+        for (int size = 0; size < 128; size++) {
+            IntFunction<Integer> memo = StableValues.memoizedIntFunction(size, i -> i);
+            for (int i = 0; i < size; i++) {
+                assertEquals(i, memo.apply(i));
+            }
+        }
+    }
+
+    @Test
+    void shakedownMultiThread() {
+        int threadCount = 8;
+        var startGate = new CountDownLatch(1);
+        for (int size = 0; size < 128; size++) {
+            IntFunction<Integer> memo = StableValues.memoizedIntFunction(size, i -> i);
+            final int fSize = size;
+            List<Thread> threads = Stream.generate(() -> new Thread(() -> {
+                        while (startGate.getCount() != 0) {
+                            Thread.onSpinWait();
+                        }
+                        for (int i = 0; i < fSize; i++) {
+                            int value = memo.apply(i);
+                            assertEquals(i, value);
+                        }
+                    }))
+                    .limit(threadCount)
+                    .toList();
+            threads.forEach(Thread::start);
+            // Give some time for the threads to start
+            LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(1));
+            // Release them as close as possible
+            startGate.countDown();
+            threads.forEach(MemoizedIntFunctionTest::join);
+        }
+    }
+
+    static void join(Thread t) {
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            fail(e);
+        }
     }
 
 }
