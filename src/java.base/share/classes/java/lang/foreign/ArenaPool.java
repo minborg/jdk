57b23aa9a99e4e08f0c453b03666372b002ae0ef
Per Minborg <pminborg@gmail.com>
2025-02-03 13:48:33 +0000

    Improve javadocs

--- src/java.base/share/classes/java/lang/foreign/ArenaPool.java
+++ src/java.base/share/classes/java/lang/foreign/ArenaPool.java
@@ -31,18 +31,36 @@ import jdk.internal.vm.annotation.ForceInline;
 import java.util.Objects;
 
 /**
- * An arena pool allows a per-thread reuse of pre-allocated memory of a certain size and
- * alignment.
+ * An arena pool that allows the reuse of pre-allocated recyclable native memory regions.
  * <p>
- * To be further expanded ...
+ * The arena pool keeps an unspecified number of recyclable memory regions in the pool
+ * where the recyclable memory regions have a certain size and alignment that is specified
+ * upon creating the pool via {@linkplain #create(MemoryLayout) one of the factory methods}.
+ * <p>
+ * The pool attempts to provide recyclable memory regions fairly across threads on a
+ * best-effort basis. The pool also tries to balance the number of recyclable memory
+ * regions held internally in the pool considering trade-offs between performance and
+ * memory allocations.
+ * <p>
+ * If an Arena allocation operation cannot be satisfied by using a recyclable memory
+ * region, new memory segments are allocated instead; allowing for graceful degradation.
  *
+ * @see Arena
  * @since 25
  */
 public sealed interface ArenaPool
         permits ArenaPoolImpl {
 
     /**
-     * {@return a new Arena that might reuse pre-allocated memory}
+     * {@return a new confined Arena that is free to use pre-allocated recyclable memory
+     *          from the pool}
+     * <p>
+     * Segments allocated with a confined arena can be
+     * {@linkplain MemorySegment#isAccessibleBy(Thread) accessed} by the thread that
+     * created the arena, the arena's <em>owner thread</em>.
+     * <p>
+     * Memory segments {@linkplain Arena#allocate(long, long) allocated} by the returned
+     * arena are zero-initialized.
      * <p>
      * Returned arenas are often short-lived as shown in this example:
      * {@snippet lang=java :
@@ -54,24 +72,31 @@ public sealed interface ArenaPool
      * }
      * }
      * <p>
-     * Memory segments {@linkplain Arena#allocate(long, long) allocated} by the returned arena
-     * are zero-initialized.
-     * <p>
-     * It is imperative that the returned Arena is closed or else pre-allocated memory
-     * may become inaccessible by the thread. For perpetual threads, this constitutes a memory leak.
-     * <p>
-     * To be further expanded ...
+     * The returned arena is free to recycle segments previously allocated and these
+     * recyclable segments may not be zero-initialized until being recycled later on.
+     * Security sensitive applications should therefore explicitly reset the content of
+     * segments before the arena is closed, as shown in this example:
+     * {@snippet lang=java :
+     * int result;
+     * try (var arena = arenaPool.take()) {
+     *     MemorySegment segment = arena.allocate(JAVA_INT);
+     *     doMemoryOperation(segment); // Writes something into the segments
+     *     result = segment.get(JAVA_INT, 0);
+     *     segment.fill((byte) 0); // Remove any security sensitive data
+     * }
+     * }
+     *<p>
+     * It is imperative that the returned Arena is closed after use or else recyclable
+     * memory may become inaccessible and might therefore constitute a memory leak.
      */
     Arena take();
 
     /**
-     * {@return a new arena pool that can return Arenas capable of recycling
-     *          memory up to the given {@code byteSize} before allocating new
-     *          memory}
-     * <p>
-     * To be further expanded ...
+     * {@return a new arena pool that is free to return Arenas capable of recycling
+     *          memory up to the given {@code byteSize} and with an alignment of 1
+     *          byte before allocating new memory}
      *
-     * @param byteSize the maximum amount of recyclable memory for a thread
+     * @param byteSize the maximum amount of recyclable memory for recyclable memory
      * @throws IllegalArgumentException if the provided {@code byteSize} is negative.
      */
     static ArenaPool create(long byteSize) {
@@ -82,11 +107,9 @@ public sealed interface ArenaPool
     }
 
     /**
-     * {@return a new arena pool that can return Arenas capable of recycling
-     *          memory up to the given {@code byteSize} before allocating new
-     *          memory}
-     * <p>
-     * To be further expanded ...
+     * {@return a new arena pool that is free to return Arenas capable of recycling
+     *          memory with the given {@code layout.byteSize()} and with an alignment of
+     *          {@code layout.byteAlignment()} before allocating new memory}
      *
      * @param layout describing the alignment and size of recyclable memory
      *
@@ -98,6 +121,9 @@ public sealed interface ArenaPool
 
     /**
      * {@return the global arena pool}
+     * <p>
+     * The global arena holds pre-allocated recyclable native memory regions
+     * of an unspecified size and alignment.
      */
     @ForceInline
     static ArenaPool global() {
